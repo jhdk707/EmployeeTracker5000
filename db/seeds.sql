920a094b0d72714500260fd5fd1ad337ff5decc6
Doomtower <jessehudak707@outlook.com>
2023-03-08 23:22:01 -0800

    fixing bugs and file name issues. still having trouble server side debugging

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,40 +3,40 @@ USE employees_db;
 INSERT INTO department (name)
 VALUES ("Sales");
 INSERT INTO department (name)
-VALUES ("Legal");
-INSERT INTO department (name)
 VALUES ("Engineering");
 INSERT INTO department (name)
 VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Salesperson", 80000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 3);
+VALUES ("Lead Engineer", 150000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 3);
+VALUES ("Software Engineer", 120000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Account Manager", 160000, 4);
+VALUES ("Account Manager", 160000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant" 125000, 4);
+VALUES ("Accountant", 125000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 250000, 2);
+VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lawyer", 190000, 2);
+VALUES ("Lawyer", 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mike", "Chan", 1, 1);
+VALUES ("Mike", "Chan", 1, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ashley", "Rodriguez", 2, 1, null);
+VALUES ("Ashley", "Rodriguez", 2, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Tupik", 1, 3);
+VALUES ("Kevin", "Tupik", 3, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kunal", "Singh", 1, 3);
+VALUES ("Kunal", "Singh", 4, 5);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Malia", "Brown", 1, 3);
+VALUES ("Malia", "Brown", 5, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sarah", "Lourd", 1, 3);
+VALUES ("Sarah", "Lourd", 6, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Allen", 1, 3);
+VALUES ("Tom", "Allen", 7, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 3);
+VALUES ("John", "Doe", 1, NULL);
